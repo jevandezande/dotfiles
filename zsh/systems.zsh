814ac2c510126a05680092b1e23cf3753a85bf15
Jonathon Vandezande <jevandezande@gmail.com>
2017-06-09 11:39:32 +0200

    Adds support for Hera

--- zsh/systems.zsh
+++ zsh/systems.zsh
@@ -5,11 +5,15 @@ then
     source $HOME/.zsh/profiles/koios.zsh
 elif ( [ $HOST = "vlogin1.ccqc.uga.edu" ] || [ $HOST = "vlogin2.ccqc.uga.edu" ] ) && [ -f profiles/vulcan.zsh ]
 then
-    source profiles/vulcan.zsh
+    source profiles/vulcan.zsh;
     source profiles/cluster.zsh
+elif [ ${HOST} = 'master1' ] || [ ${HOST} = 'master2' ]
+then
+    source ~/.zsh/profiles/hera.zsh;
+    source ~/.zsh/profiles/cluster.zsh
 elif [ ${HOST:0:3} = 'uga' ] || [ ${HOST: -13} = 'gacrc.uga.edu' ]
 then
-    source profiles/sapelo.zsh
+    source profiles/sapelo.zsh;
     source profiles/cluster.zsh
 elif [ -n "$HOST" ]
 then
