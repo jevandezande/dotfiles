330ddcdefd8ae7f830ef2929f4f41967f7cbc568
Jonathon Vandezande <jevandezande@gmail.com>
2017-06-22 11:13:57 +0200

    Adds cluster aliases.

--- zsh/profiles/cluster.zsh
+++ zsh/profiles/cluster.zsh
@@ -15,3 +15,12 @@ alias qme='qstat -u `whoami`'
 alias qall='qstat -f -u "*"'
 alias qu='qinfo -u'
 alias qw='qinfo -uw'
+finished () { tail ~/.jobs -n ${1:-10} }
+
+
+##########
+# Random #
+##########
+# delete e and o files
+alias rmeo='ls | grep ".*\.[eo][0-9]\+" | xargs rm'
+alias cdj='qstat -f $1 | grep "PBS_O_WORKDIR" | sed -e "s|.*PBS_O_WORKDIR=\([^,]*\),.*|\1|"'
