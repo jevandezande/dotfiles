aefc00e0e42594a6ce5d3c2f7c7f999366549022
Jonathon Vandezande <jevandezande@gmail.com>
2017-06-29 10:25:10 +0200

    Adds aout and cout aliases.

--- zsh/profiles/koios.zsh
+++ zsh/profiles/koios.zsh
@@ -8,3 +8,4 @@ export PATH="$PATH:/home/vandezande/progs/openchemistry-build/prefix/bin/"
 # Transfer #
 ############
 alias cpo='scp zeus:output.dat .'
+alias cview='scp zeus:output.dat . && Chemcraft output.dat'
