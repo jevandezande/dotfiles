0bafc40ac355dca05d1bb5573b55ab0b26a64573
Jonathon Vandezande <jevandezande@gmail.com>
2017-07-03 14:43:31 +0200

    Updates cdj and jobs functions.

--- zsh/profiles/cluster.zsh
+++ zsh/profiles/cluster.zsh
@@ -15,7 +15,7 @@ alias qme='qstat -u `whoami`'
 alias qall='qstat -f -u "*"'
 alias qu='qinfo -u'
 alias qw='qinfo -uw'
-finished () { tail ~/.jobs -n ${1:-10} }
+jobs () { tail ~/.jobs -n ${1:-10} }
 
 
 ##########
@@ -23,4 +23,26 @@ finished () { tail ~/.jobs -n ${1:-10} }
 ##########
 # delete e and o files
 alias rmeo='ls | grep ".*\.[eo][0-9]\+" | xargs -r rm'
-alias cdj='qstat -f $1 | grep "PBS_O_WORKDIR" | sed -e "s|.*PBS_O_WORKDIR=\([^,]*\),.*|\1|"'
+
+cdj () {
+	# cd to the appropriate job, whether running or finished
+	# Find the job in qstat
+	job=$(qstat $1 -f 2> /dev/null)
+	# If the job string is not empty
+	if [[ -n $job ]]
+	then
+		# cut out the directory and cd to the job
+		cd $(echo $job | grep "PBS_O_WORKDIR" | sed -e "s|.*PBS_O_WORKDIR=\([^,]*\),.*|\1|")
+	else
+		# Try looking for it in ~/.jobs
+		job=$(tac ~/.jobs | grep "^$1: " -m 1)
+		if [[ -n $job ]]
+		then
+			# cut out the directory and cd to the job
+			# (does not work with spaces in job name or directory)
+			cd $(echo $job | cut -f4 -d ' ')
+		else
+			echo "Could not find job"
+		fi
+	fi
+}
