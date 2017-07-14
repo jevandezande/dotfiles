6670d45ecaf8aa90b898a7166637a91c6578f4a1
Jonathon Vandezande <jevandezande@gmail.com>
2017-07-14 09:49:40 +0200

    Adds alias to rsync with Zeus.

--- zsh/profiles/koios.zsh
+++ zsh/profiles/koios.zsh
@@ -1,6 +1,8 @@
 # Prompt style
 prompt adam1 blue
 
+source $HOME/.zsh/profiles/local.zsh
+
 export PATH="$PATH:/home/vandezande/progs/openchemistry-build/prefix/bin/"
 
 
---
+++ zsh/profiles/local.zsh
@@ -0,0 +1,2 @@
+# Sync from zeus; note: rsync works on first matching pattern (thus include must be first)
+alias zsync='rsync zeus: ~/tmp/zeus/ --delete -azP --include={input.dat} --exclude={"input.*","*.tar.gz","*.tgz","*.tbz",tmp/,progs/,old/,".*"}'
