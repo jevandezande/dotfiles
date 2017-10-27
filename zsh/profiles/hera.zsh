499be32fb838aa60c4ca462dfa4ab70969b1b95a
Jonathon Vandezande <jevandezande@gmail.com>
2017-10-27 11:44:43 +0200

    Adds hera profile.

---
+++ zsh/profiles/hera.zsh
@@ -0,0 +1,4 @@
+# Prompt style
+export PROMPT='%F{green}jev@hera%f% %~$ '
+
+export PATH=$PATH:'/home1/vandezande/.install/rcm/bin'
--- zsh/systems.zsh
+++ zsh/systems.zsh
@@ -2,25 +2,25 @@
 
 if [ $HOST = "pccec0668" ]
 then
-    source $HOME/.zsh/profiles/koios.zsh
+    source ~/.zsh/profiles/koios.zsh
 elif ( [ $HOST = "vlogin1.ccqc.uga.edu" ] || [ $HOST = "vlogin2.ccqc.uga.edu" ] ) && [ -f profiles/vulcan.zsh ]
 then
-    source profiles/vulcan.zsh;
-    source profiles/cluster.zsh
+    source ~/.zsh/profiles/vulcan.zsh
+    source ~/.zsh/profiles/cluster.zsh
 elif [ ${HOST} = 'master1' ] || [ ${HOST} = 'master2' ]
 then
-    source ~/.zsh/profiles/null_hostname.zsh;
-    source ~/.zsh/profiles/mpi.zsh;
+    source ~/.zsh/profiles/hera.zsh
+    source ~/.zsh/profiles/mpi.zsh
     source ~/.zsh/profiles/cluster.zsh
 elif [ ${HOST:0:6} = 'zeusln' ]
 then
-    source ~/.zsh/profiles/zeus.zsh;
-    source ~/.zsh/profiles/mpi.zsh;
+    source ~/.zsh/profiles/zeus.zsh
+    source ~/.zsh/profiles/mpi.zsh
     source ~/.zsh/profiles/cluster.zsh
 elif [ ${HOST:0:3} = 'uga' ] || [ ${HOST: -13} = 'gacrc.uga.edu' ]
 then
-    source profiles/sapelo.zsh;
-    source profiles/cluster.zsh
+    source ~/.zsh/profiles/sapelo.zsh
+    source ~/.zsh/profiles/cluster.zsh
 elif [ -n "$HOST" ]
 then
     echo "Cannot find the proper host and associated system profile. Host: $HOST"
