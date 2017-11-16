feb8e3cb272900998c3d6a0eb9794d9f796a1d80
Jonathon Vandezande <jevandezande@gmail.com>
2017-11-16 15:33:16 +0100

    Adds cluster to PYTHONPATH.

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -5,7 +5,8 @@
 export PYTHONPATH=$PYTHONPATH:~/progs/qgrep/
 # CCLib
 export PYTHONPATH=$PYTHONPATH:~/progs/cclib/src/
-
+# Cluster
+export PYTHONPATH=$PYTHONPATH:~/progs/cluster
 
 # Go
 export PATH=$PATH:~/progs/go/bin/
