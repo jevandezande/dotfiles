0fedbd21b7025e2769878e2950fc733dc7400442
Jonathon Vandezande <jevandezande@gmail.com>
2017-11-16 15:33:36 +0100

    Adds aview with Avogadro (not 2).

--- zsh/profiles/koios.zsh
+++ zsh/profiles/koios.zsh
@@ -11,4 +11,5 @@ export PATH="$PATH:/home/vandezande/progs/openchemistry-build/prefix/bin/"
 ############
 alias cpo='scp zeus:output.dat .'
 alias aout='scp zeus:output.dat . && avogadro2 output.dat'
+alias aview='scp zeus:output.dat . && get_geom -i output.dat && avogadro geom.xyz'
 alias cout='scp zeus:output.dat . && Chemcraft output.dat'
