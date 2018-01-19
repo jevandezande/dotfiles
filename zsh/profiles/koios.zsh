e0660767f58ff634805c9c7ff5c16709c5a160cc
Jonathon Vandezande <jevandezande@gmail.com>
2018-01-19 14:00:15 +0100

    Rearrange location of commands.

--- zsh/profiles/koios.zsh
+++ zsh/profiles/koios.zsh
@@ -1,15 +1,6 @@
 # Prompt style
 export PROMPT='%F{blue}jev@koios%f% %~$ '
 
-
 source $HOME/.zsh/profiles/local.zsh
 
 export PATH="$PATH:/home/vandezande/progs/openchemistry-build/prefix/bin/"
-
-############
-# Transfer #
-############
-alias cpo='scp zeus:output.dat .'
-alias aout='scp zeus:output.dat . && avogadro2 output.dat'
-alias aview='scp zeus:output.dat . && get_geom -i output.dat && avogadro geom.xyz'
-alias cout='scp zeus:output.dat . && Chemcraft output.dat'
--- zsh/profiles/local.zsh
+++ zsh/profiles/local.zsh
@@ -2,3 +2,11 @@
 alias zsync='rsync zeus: ~/tmp/zeus/ --delete -azP --include=input.dat --exclude={"input.*","*.tar.gz","*.tgz","*.tbz",tmp/,progs/,old/,".*"}'
 
 alias mrv='MarvinSketch $1 2> /dev/null'
+
+############
+# Transfer #
+############
+alias cpo='scp zeus:output.dat .'
+alias aout='scp zeus:output.dat . && avogadro2 output.dat'
+alias aview='scp zeus:output.dat . && get_geom -i output.dat && avogadro geom.xyz'
+alias cout='scp zeus:output.dat . && Chemcraft output.dat'
