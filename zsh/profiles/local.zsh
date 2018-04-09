619ce900a48708261f86fb72da2b0794781d3e41
Jonathon Vandezande <jevandezande@gmail.com>
2018-04-09 17:14:36 +0200

    Adds cfour alias.

--- zsh/profiles/local.zsh
+++ zsh/profiles/local.zsh
@@ -4,8 +4,18 @@ alias zsync='rsync zeus: ~/tmp/zeus/ --delete -azP --include=input.dat --exclude
 alias mrv='MarvinSketch $1 2> /dev/null'
 
 ################
+# File Copying #
+################
+alias gcz='scp geom.xyz zeus:'
+
+################
 # File Viewing #
 ################
 alias aout='avogadro2 output.dat'
 alias aview='get_geom -i output.dat && avogadro geom.xyz'
 alias cout='cheMVP output.dat'
+
+###################
+# Program Running #
+###################
+alias cfour_run='xcfour ZMAT > output.dat 2> /dev/null & disown'
