8a10d2a37ef9b53194d565cc12c00be9fd0178db
Jonathon Vandezande <jevandezande@gmail.com>
2018-04-26 14:08:23 +0200

    Fixes some paths.

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -2,16 +2,16 @@
 # Python #
 ##########
 # QGrep
-export PYTHONPATH=$PYTHONPATH:~/progs/qgrep/
+export PYTHONPATH=$PYTHONPATH:~/progs/qgrep
 # CCLib
-export PYTHONPATH=$PYTHONPATH:~/progs/cclib/src/
+export PYTHONPATH=$PYTHONPATH:~/progs/cclib
 # Cluster
 export PYTHONPATH=$PYTHONPATH:~/progs/cluster
 # CFour
 export PATH=$PATH:~/.install/cfour/bin
 
 # Go
-export PATH=$PATH:~/progs/go/bin/
-export GOPATH=~/.go
+export GOPATH=~/work/go
+export PATH=$PATH:~/work/go/bin
 
 export PATH=$PATH:~/progs/miniconda3/bin
