d05d4505023254fd509c4f8d017cc07580b1c0ae
Jonathon Vandezande <jevandezande@gmail.com>
2018-04-30 13:52:49 +0200

    Adds zplug.

--- zsh/setup.zsh
+++ zsh/setup.zsh
@@ -2,6 +2,8 @@ source $HOME/.zsh/basic_setup.zsh
 
 source $HOME/.zsh/general_aliases.zsh
 
+source $HOME/.zsh/zplug.zsh
+
 source $HOME/.zsh/paths.zsh
 
 source $HOME/.zsh/systems.zsh
---
+++ zsh/zplug.zsh
@@ -0,0 +1,16 @@
+source /usr/share/zplug/init.zsh
+
+# Autoswitching virtualenv
+zplug "MichaelAquilina/zsh-autoswitch-virtualenv"
+source =virtualenvwrapper.sh
+
+# Install plugins if there are plugins that have not been installed
+ if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+	if read -q; then
+		echo; zplug install
+	fi
+fi
+
+# Then, source plugins and add commands to $PATH
+zplug load  #--verbose
