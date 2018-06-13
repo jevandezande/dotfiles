8fb1100a7628a53ded96363929df802f24c98af7
Jonathon Vandezande <jevandezande@gmail.com>
2018-06-13 14:42:32 +0200

    zsync -> cluster_sync

--- zsh/profiles/local.zsh
+++ zsh/profiles/local.zsh
@@ -1,5 +1,22 @@
-# Sync from zeus; note: rsync works on first matching pattern (thus include must be first)
-alias zsync='rsync zeus: ~/tmp/zeus/ --delete -azP --include=input.dat --exclude={"input.*","*.tar.gz","*.tgz","*.tbz",tmp/,progs/,old/,".*"}'
+################
+# Cluster Sync #
+################
+cluster_sync () {
+    if [ -z $1 ]
+    then
+        echo "Please choose from a cluster in"
+        ls ~/tmp/sync
+    else
+        echo "Syncing $1"
+        # note: rsync works on first matching pattern (thus include must be first)
+        rsync $1: \
+            ~/tmp/sync/$1/ \
+            -azP \
+            --delete \
+            --include=input.dat \
+            --exclude={"input.*","*.tar.gz","*.tgz","*.tbz",tmp/,progs/,old/,".*"}
+    fi
+}
 
 alias mrv='MarvinSketch $1 2> /dev/null'
 
