b619e294445dbc0c0a6df6f1ee0e2ba393e33140
Jonathon Vandezande <jevandezande@gmail.com>
2019-03-01 09:30:05 -0500

    Fixes zplug install.

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,15 +1,21 @@
 source ~/.zplug/init.zsh
 
+
 # Autoswitching virtualenv
+export VIRTUALENVWRAPPER_PYTHON=python3
+export WORKON_HOME=~/.venv
+mkdir -p $WORKON_HOME
+export PROJECT_HOME=~/dev
+mkdir -p $WORKON_HOME
 zplug "MichaelAquilina/zsh-autoswitch-virtualenv"
-source =virtualenvwrapper.sh
+source ~/.local/bin/virtualenvwrapper.sh
 
 # Install plugins if there are plugins that have not been installed
  if ! zplug check --verbose; then
     printf "Install? [y/N]: "
-	if read -q; then
-		echo; zplug install
-	fi
+    if read -q; then
+        echo; zplug install
+    fi
 fi
 
 # Then, source plugins and add commands to $PATH
