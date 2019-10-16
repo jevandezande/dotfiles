af5394aed62f6b186c3b1dfb2261b36bbe8586ed
Jonathon Vandezande <jevandezande@gmail.com>
2019-10-16 11:55:40 -0400

    Using Google Drive File Stream now.

--- zsh/profiles/1st_source.zsh
+++ zsh/profiles/1st_source.zsh
@@ -1,4 +1,5 @@
 hash -d jonat='/mnt/c/Users/jonat'
-hash -d Google_Drive='/mnt/c/Users/jonat/Google_Drive'
-hash -d 1st='/mnt/c/Users/jonat/Google_Drive/1st Source Research'
-hash -d Zymergen='/mnt/c/Users/jonat/Google_Drive/1st Source Research/Customers/Zymergen'
+hash -d Downloads='/mnt/c/Users/jonat/Downloads'
+
+export jonat='/mnt/c/Users/jonat'
+export down='/mnt/c/Users/jonat/Downloads'
