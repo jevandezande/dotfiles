2439540a6bfc1d2dc1bcebf413a382df4564f728
Jonathon Vandezande <jevandezande@gmail.com>
2020-08-18 10:06:09 -0400

    Adds conda for 1sr_mpi.

--- zsh/profiles/1sr_mpi.zsh
+++ zsh/profiles/1sr_mpi.zsh
@@ -1,1 +1,14 @@
 prompt adam1 green
+
+
+__conda_setup="$('~/progs/miniconda/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval $__conda_setup
+else
+    if [ -f ~/miniconda/etc/profile.d/conda.sh ]; then
+        . ~/miniconda/etc/profile.d/conda.sh
+    else
+        export PATH=~/progs/miniconda/bin:$PATH
+    fi
+fi
+unset __conda_setup
