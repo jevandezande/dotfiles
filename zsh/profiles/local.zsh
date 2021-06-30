df30229e32bafce3d2bbe499b8035d656892a051
Jonathon Vandezande <jevandezande@gmail.com>
2021-06-30 11:27:40 -0400

    Cleanup before getting new computer

--- zsh/profiles/local.zsh
+++ zsh/profiles/local.zsh
@@ -2,25 +2,15 @@
 # Cluster Sync #
 ################
 cluster_sync () {
-    cluster=${1-sch}
-    if [ -z $cluster ]
-    then
-        echo "Please choose from a cluster in"
-        ls ~/tmp/sync
-    else
-        echo "Syncing $cluster"
-        # note: rsync works on first matching pattern (thus include must be first)
-        rsync $cluster: \
-            ~/tmp/sync/$cluster/ \
-            -azP \
-            --delete \
-            #--include=input.dat \
-            --exclude={
-                "input.*","*.tar.gz","*.tgz","*.tbz",
-                "job.log","job.recover","job.01.mae",
-                tmp/,progs/,old/,wekafiles,
-                ".*",}
-    fi
+    cluster=${$1:cronos}
+    echo "Syncing $1"
+    # note: rsync works on first matching pattern (thus include must be first)
+    rsync $1: \
+        ~/tmp/sync/$1/ \
+        -azP \
+        --delete \
+        --include=input.dat \
+        --exclude={"input.*","*.tar.gz","*.tgz","*.tbz",tmp/,progs/,old/,".*"}
 }
 
 alias mrv='MarvinSketch $1 2> /dev/null'
