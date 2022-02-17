0b8f22d852b5886b70cdb92b7e4d2217dfef9cdd
Jonathon Vandezande <jevandezande@gmail.com>
2022-02-17 18:05:23 -0500

    Removes unnecessary paths.

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -4,19 +4,5 @@
 export PATH=~/.local/bin:$PATH
 export PATH=$HOME/.bin:$PATH
 
-##########
-# Python #
-##########
-# QGrep
-export PYTHONPATH=$PYTHONPATH:~/progs/qgrep
-# Cluster
-export PYTHONPATH=$PYTHONPATH:~/progs/cluster
-# Reaction Web
-export PYTHONPATH=$PYTHONPATH:~/progs/reaction_web
-
-
 # Rust
 export PATH=$PATH:~/.cargo/bin
-
-# QGrep
-export PATH=$PATH:~/progs/qgrep/bin
