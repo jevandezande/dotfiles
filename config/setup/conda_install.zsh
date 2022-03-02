22d0ae1366eebc3adee200716da9de6bed712335
Jonathon Vandezande <jevandezande@gmail.com>
2022-03-02 09:26:31 -0500

    Removes extraneous conda_install.zsh and comments.

--- config/setup/conda_install.zsh
+++ config/setup/conda_install.zsh
@@ -1,9 +1,9 @@
 #!/usr/bin/env zsh
 
-echo "Conda!"
-conda_dir=~/progs/test/anaconda3
+conda_dir=~/progs/anaconda3
 conda=$conda_dir/bin/conda
 mamba=$conda_dir/bin/mamba
+
 if [ ! -d $conda_dir ]
 then
     OS=""
@@ -17,16 +17,11 @@ then
         echo "Failed to determine OS: $OS"
         exit 0
     fi
-    curl -o anaconda_install.sh "https://repo.anaconda.com/archive/Anaconda3-2021.05-$OS-x86_64.sh"
+    # Miniconda: "https://repo.anaconda.com/miniconda/Miniconda3-latest-$OS-x86_64.sh"
+    curl -o anaconda_install.sh "https://repo.anaconda.com/archive/Anaconda3-2021.11-$OS-x86_64.sh"
     bash anaconda_install.sh -b -p $conda_dir
     rm anaconda_install.sh
-
-    #    curl -o Miniconda3-latest.sh "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
-    #    bash Miniconda3-latest.sh -b -p $conda_dir
-    #    rm Miniconda3-latest.sh
 fi
-
-echo 2
 
 $conda update -n base -c defaults conda --yes
 $conda update --all --yes
@@ -40,6 +35,3 @@ do
     echo $env
     $mamba env create -f $env
 done
-
-#jupyter-nbextension enable nglview --py --sys-prefix
-#jupyter labextension install nglview-js-widgets
