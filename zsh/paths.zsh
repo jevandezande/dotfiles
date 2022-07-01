98b74021f42a5e6734ade4e61321cc38e0dc8162
Jonathon Vandezande <jevandezande@gmail.com>
2022-07-01 14:26:06 -0400

    Adds Linuxbrew paths.

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -6,3 +6,13 @@ export PATH=$HOME/.bin:$PATH
 
 # Rust
 export PATH=$PATH:~/.cargo/bin
+
+if [ $(uname) = "Linux" ]
+    # Linuxbrew
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)" eval export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+    export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar"
+    export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew"
+    export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin${PATH+:$PATH}"
+    export MANPATH="/home/linuxbrew/.linuxbrew/share/man${MANPATH+:$MANPATH}:"
+    export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}"
+fi
