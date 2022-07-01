5b035433e4638197153a80864f8f51206283149d
Jonathon Vandezande <jevandezande@gmail.com>
2022-07-01 14:24:22 -0400

    Removes excess snaps.

--- config/setup/snap_install.zsh
+++ config/setup/snap_install.zsh
@@ -2,14 +2,12 @@
 #########################
 # Install snap programs #
 #########################
+
 snap_progs=(
-    firefox             # Browser
     gimp                # Raster graphic editor
     inkscape            # Vector graphics editor
     keepassxc           # Password storage
-    pycharm-community   # Python IDE
     spotify             # Music player
-    valgrind            # Code checker
 )
 
 for prog in "${snap_progs[@]}"
