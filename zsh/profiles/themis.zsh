087bd21e903732c51c83283621b57b2dc182e1a3
Jonathon Vandezande <jevandezande@gmail.com>
2023-02-02 14:59:43 -0500

    Fixes file permissions

--- zsh/profiles/themis.zsh
+++ zsh/profiles/themis.zsh
@@ -3,3 +3,5 @@ hash -d down=/mnt/c/Users/vandezan/Downloads/
 hash -d work=/mnt/c/Users/vandezan/Documents/work
 hash -d elec=/mnt/c/Users/vandezan/Documents/work/Electrolytes
 hash -d rw=/mnt/c/Users/vandezan/Documents/work/rw
+hash -d rnco=/mnt/c/Users/vandezan/Documents/work/RNCO
+hash -d dnco=/mnt/c/Users/vandezan/Documents/work/RNCO/Diisocyanates
