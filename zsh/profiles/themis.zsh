eb5346084dcccc3f8d571f4decda157df120131b
Jonathon Vandezande <jevandezande@gmail.com>
2023-04-18 11:25:04 -0400

    Moved a directory.

--- zsh/profiles/themis.zsh
+++ zsh/profiles/themis.zsh
@@ -4,4 +4,4 @@ hash -d work=/mnt/c/Users/vandezan/Documents/work
 hash -d elec=/mnt/c/Users/vandezan/Documents/work/Electrolytes
 hash -d rw=/mnt/c/Users/vandezan/Documents/work/rw
 hash -d rnco=/mnt/c/Users/vandezan/Documents/work/RNCO
-hash -d dnco=/mnt/c/Users/vandezan/Documents/work/RNCO/Diisocyanates
+hash -d dnco=/mnt/c/Users/vandezan/Documents/work/RNCO/DNCO
