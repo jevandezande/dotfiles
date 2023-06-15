0c8d2ca350be3c049ba53418d8a5e2e56362ff18
Jonathon Vandezande <jevandezande@gmail.com>
2023-06-15 17:26:33 -0400

    Updates Schrodinger version.

--- zsh/profiles/schrodinger.zsh
+++ zsh/profiles/schrodinger.zsh
@@ -1,1 +1,4 @@
-export SCHRODINGER="/home/jevandezande/progs/schrodinger/22-4"
+MAESTRO_VERSION="2023-3"
+BUILD="069"
+export SCHRODINGER="/home/jevandezande/progs/schrodinger/$MAESTRO_VERSTION/$BUILD"
+export WIN_SCHRO="/mnt/c/Program Files/Schrodinger$MAESTRO_VERSTION"
