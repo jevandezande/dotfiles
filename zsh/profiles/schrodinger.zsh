f9111bc3e27f6651383aca58cfdd9f94cf283f11
Jonathon Vandezande <jevandezande@gmail.com>
2023-08-02 15:26:21 -0400

    Automagically find latest Schrodinger version.

--- zsh/profiles/schrodinger.zsh
+++ zsh/profiles/schrodinger.zsh
@@ -1,4 +1,11 @@
-MAESTRO_VERSION="2023-3"
-BUILD="069"
-export SCHRODINGER="/home/jevandezande/progs/schrodinger/$MAESTRO_VERSTION/$BUILD"
-export WIN_SCHRO="/mnt/c/Program Files/Schrodinger$MAESTRO_VERSTION"
+# Find latest Schrödinger version
+location=~/progs/schrodinger
+latest_version=`ls $location/ -1 | tail -n 1`
+latest_build=`ls $location/$latest_version -1 | tail -n 1`
+export SCHRODINGER=$location/$latest_version/$latest_build
+
+if [[ $(grep microsoft /proc/version) ]]
+then
+  location="/mnt/c/Program Files"
+  export WIN_SCHRO=`ls $location/Schrodinger* -1 -d | tail -n 1`
+fi
