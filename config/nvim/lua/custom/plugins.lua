e400da2cf511cdf240465e413c6835223fe8929b
Jonathon Vandezande <jevandezande@gmail.com>
2023-09-26 11:18:47 -0400

    Fixes cmp error.

--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -19,6 +19,20 @@ local plugins = {
     },
   },
   {
+    "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "pass show api/tokens/openai",
+      })
+    end,
+  },
+  {
     "neovim/nvim-lspconfig",
     config = function()
       require "plugins.configs.lspconfig"
@@ -105,7 +119,7 @@ local plugins = {
     config = function(_, opts)
       local crates  = require('crates')
       crates.setup(opts)
-      require('cmp').setup.buffer({
+      cmp.setup.buffer({
         sources = { { name = "crates" }}
       })
       crates.show()
@@ -115,14 +129,14 @@ local plugins = {
   {
     "hrsh7th/nvim-cmp",
     opts = function()
-      -- local M = require "plugins.configs.cmp" -- M -> cmp
-      cmp.completion.completeopt = "menu,menuone,noselect"
-      cmp.mapping["<CR>"] = cmp.mapping.confirm {
+      local M = require "plugins.configs.cmp"
+      M.completion.completeopt = "menu,menuone,noselect"
+      M.mapping["<CR>"] = cmp.mapping.confirm {
         behavior = cmp.ConfirmBehavior.Insert,
         select = false,
       }
-      table.insert(cmp.sources, {name = "crates"})
-      return cmp
+      table.insert(M.sources, {name = "crates"})
+      return M
     end,
   }
 }
