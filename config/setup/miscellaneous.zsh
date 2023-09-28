15093988ed8dea8ba57b12351a41ab2010788fad
Jonathon Vandezande <jevandezande@gmail.com>
2023-09-28 10:57:50 -0400

    Moves NvChad setup outside of dotfiles.
        NvChad is now directly installed in ~/.config/nvim
        Custom setup is still available at config/nvim/lua/custom

--- config/setup/miscellaneous.zsh
+++ config/setup/miscellaneous.zsh
@@ -8,3 +8,10 @@ then
   echo "Installing rustup"
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
+
+
+activate-global-python-argcomplete
+
+
+# Install NvChad
+git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
