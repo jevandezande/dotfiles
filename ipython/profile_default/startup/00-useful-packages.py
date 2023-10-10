f95217deca7b935906e46da5e689daba6be846a5
Jonathon Vandezande <jevandezande@gmail.com>
2023-10-10 10:45:19 -0400

    Print ipython default imports.

--- ipython/profile_default/startup/00-useful-packages.py
+++ ipython/profile_default/startup/00-useful-packages.py
@@ -1,41 +1,18 @@
-#from importlib import import_module
-#
-## module name: Optional[what to rename it]
-#packages = {
-#    "matplotlib": None,
-#    "matplotlib.pyplot": "plt",
-#    "numpy": "np",
-#    "pandas": "pd",
-#    "scipy": "sp",
-#    "sympy": "sym",
-#}
-#
-# print("Loading:", end="")
-#for name, rename in packages.items():
-#    if not rename:
-#        rename = name
-#    rename = import_module(package)
-#    print(f" {name} -> {rename},", end="")
+from importlib import import_module
 
-import matplotlib
-import matplotlib.pyplot as plt
-import numpy as np
+packages: dict[str, str | None] = {
+    "matplotlib": None,
+    "matplotlib.pyplot": "plt",
+    "numpy": "np",
+    "pandas": "pd",
+    "scipy": "sp",
+    "sympy": "sym",
+}
 
-try:
-    pass
-    import pandas as pd
-except NameError:
-    pass
-try:
-    pass
-    import scipy as sp
-except NameError:
-    pass
-
-try:
-    import sympy as sym
-    sympy.init_printing()
-except NameError:
-    pass
-
-# Setup
+for name, rename in packages.items():
+    mod = import_module(name)
+    globals()[rename or name] = mod
+    if rename:
+        print(f"{name} -> {rename}")
+    else:
+        print(f"{name}")
