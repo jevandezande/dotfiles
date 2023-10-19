4d6601c6fb0fe273f531e8b579d907dc13b6c89f
Jonathon Vandezande <jevandezande@gmail.com>
2023-10-19 15:25:37 -0400

    Adds relativenumber to NeoVim

--- config/nvim/lua/custom/chadrc.lua
+++ config/nvim/lua/custom/chadrc.lua
@@ -1,6 +1,8 @@
+require "settings"
+
 ---@type ChadrcConfig 
- local M = {}
- M.ui = {theme = 'catppuccin'}
- M.plugins = "custom.plugins"
- M.mappings = require "custom.mappings"
- return M
+local M = {}
+M.ui = {theme = 'catppuccin'}
+M.plugins = "custom.plugins"
+M.mappings = require "custom.mappings"
+return M
