001bdba28b2f30b3bf673fcb9b948a56e06eea26
Jonathon Vandezande <jevandezande@gmail.com>
2023-11-28 15:22:28 -0500

    Adds staship configuration for SCHRODINGER

--- zsh/profiles/schrodinger.zsh
+++ zsh/profiles/schrodinger.zsh
@@ -1,10 +1,10 @@
 # Find latest Schrödinger version
 location=~/progs/schrodinger
 # Only look for directories beginning with 20
-schrodinger_versions=($(find $location/20* -maxdepth 0 -type d -exec basename {} \;))
-latest_schrodinger_version=$schrodinger_versions[-1]
-latest_schrodinger_builds=($(find $location/$latest_schrodinger_version/* -maxdepth 0 -type d -exec basename {} \;))
-latest_schrodinger_build=$latest_schrodinger_builds[-1]
+export schrodinger_versions=($(find $location/20* -maxdepth 0 -type d -exec basename {} \;))
+export latest_schrodinger_version=$schrodinger_versions[-1]
+export latest_schrodinger_builds=($(find $location/$latest_schrodinger_version/* -maxdepth 0 -type d -exec basename {} \;))
+export latest_schrodinger_build=$latest_schrodinger_builds[-1]
 export SCHRODINGER=$location/$latest_schrodinger_version/$latest_schrodinger_build
 
 if [[ $(grep microsoft /proc/version) ]]
