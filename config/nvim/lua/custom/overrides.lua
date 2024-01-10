2e904c324a99b110cf05c07e2ce3e2705650f71a
Jonathon Vandezande <jevandezande@gmail.com>
2024-01-10 16:28:26 -0500

    Fixes i3 screenshot filename.

---
+++ config/nvim/lua/custom/overrides.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+M.copilot = {
+  -- Possible configurable fields can be found on:
+  -- https://github.com/zbirenbaum/copilot.lua#setup-and-configuration
+  panel = {
+    enabled = true,
+    auto_refresh = false,
+    keymap = {
+      jump_prev = "[[",
+      jump_next = "]]",
+      accept = "<CR>",
+      refresh = "gr",
+      open = "<M-CR>"
+    },
+    layout = {
+      position = "right", -- | top | left | right
+      ratio = 0.4
+    },
+  },
+  suggestion = {
+    enabled = true,
+    auto_trigger = true,
+    keymap = {
+      accept = "<F1>",
+      accept_word = false,
+      accept_line = false,
+      next = "<F2>",
+      prev = "<F3>",
+      dismiss = "<C-\\>",
+    },
+  },
+}
+
+return M
--- config/nvim/lua/custom/plugins.lua
+++ config/nvim/lua/custom/plugins.lua
@@ -1,4 +1,5 @@
 local cmp = require "cmp"
+local overrides = require("custom.overrides")
 
 local plugins = {
   {
@@ -18,20 +19,20 @@ local plugins = {
       },
     },
   },
-  {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      require("chatgpt").setup({
-        api_key_cmd = "pass show api/tokens/openai",
-      })
-    end,
-  },
+  -- {
+  --   "jackMort/ChatGPT.nvim",
+  --   event = "VeryLazy",
+  --   dependencies = {
+  --     "MunifTanjim/nui.nvim",
+  --     "nvim-lua/plenary.nvim",
+  --     "nvim-telescope/telescope.nvim",
+  --   },
+  --   config = function()
+  --     require("chatgpt").setup({
+  --       api_key_cmd = "pass show api/tokens/openai",
+  --     })
+  --   end,
+  -- },
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -158,5 +159,33 @@ local plugins = {
   {
     "lervag/vimtex",
   },
+  {
+    "zbirenbaum/copilot.lua",
+    event = "InsertEnter",
+    opts = overrides.copilot,
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "zbirenbaum/copilot-cmp",
+        config = function()
+          require("copilot_cmp").setup()
+        end,
+      },
+    },
+    opts = {
+      sources = {
+        { name = "nvim_lsp", group_index = 2 },
+        { name = "copilot",  group_index = 2 },
+        { name = "luasnip",  group_index = 2 },
+        { name = "buffer",   group_index = 2 },
+        { name = "nvim_lua", group_index = 2 },
+        { name = "path",     group_index = 2 },
+      },
+    },
+  },
 }
+
 return plugins
