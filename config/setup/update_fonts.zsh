f80ce7a98bcd02ecae99839cccaf1373f0b4b956
Jonathon Vandezande <jevandezande@gmail.com>
2024-08-08 18:45:09 -0400

    Install more fonts

--- config/setup/update_fonts.zsh
+++ config/setup/update_fonts.zsh
@@ -16,6 +16,17 @@ then
   mkdir -p $dir
 fi
 
+
+repo="https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts"
 pushd $dir
-  curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
+  # Droid Sans Mono
+  curl -fLO $repo/DroidSansMono/DroidSansMNerdFont-Regular.otf
+
+  # Meslo
+  font_family="Meslo/"
+  something="S"
+  for emphasis in Bold Italic Regular Bold-Italic
+  do
+    curl -fLO $repo/$font_family/$something/$emphasis/MesloLG${something}NerdFontMono-$emphasis.ttf
+  done
 popd
